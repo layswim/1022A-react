8050b02426ca5d85216239524b53ad701aa64f73
layswimm <lays.zifirino@estudante.ifms.edu.br>
2024-03-12 10:09:22 -0400

    Meu Primeiro Commit

---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,18 @@
+//faça um componente que faça um retângulo da largura da tela por 200px
+//crie uma variavel com LET no js que contenha o valor "Meu Componente"
+//Crie uma div no componente que dentro dela mostre um parágrafo<p> contendo o valor da variável
+
+
+import './MeuComponente.css'
+function MeuComponente(){
+    let variavel = "Meu Componente"
+    return(
+<div className= "minha-div" > 
+<p>{variavel}</p>
+</div>
+    )
+}
+    export default MeuComponente;
+    
+   //JSX - Javascript XML
+  
