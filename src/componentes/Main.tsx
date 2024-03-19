104cb12956721ef1aec3c9e51c3be7d7f83a121f
layswim <118370938+layswim@users.noreply.github.com>
2024-03-19 10:14:31 -0400

    box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,18 @@
+import './Main.css'
 function Main(){
     return(
-        <>
-    <main>Main</main>
-    <h1>meu h1</h1>
-    </>
+        <main>
+            <div className="estudante-box"> 
+                <img className='estudante-foto' src= "https://github.com/layswim.png" alt=""/>
+                <div className="informacoes">
+                    <h3 className="estudante_nome">Lays Wimmer</h3>
+                    <p className="estudante_descricao">Aluna Instituto Federal</p>
+                    <a className='link-git' href="https://github.com/layswim">GitHub</a>
+            
+                </div>
+            </div>
+        </main>
     ) 
 }
+
 export default Main;
